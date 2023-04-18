9a26dd5b5acba97247857aa4554f5de639ff3ba7
Yerkebulan <128957554+yerkebulan0@users.noreply.github.com>
2023-04-18 20:54:01 +0600

    code clean

--- index.js
+++ index.js
@@ -1,47 +1,33 @@
-const model = [{
-    title:"",
-    caption:"You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.",
-    topic:"JavaScript",
-    date:"7 july",
-    author:"erke"
-},{
-    title:"",
-    caption:"You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.",
-    topic:"JavaScript",
-    date:"7 july",
-    author:"erke"
-},{
-    title:"",
-    caption:"You not only learn more Python by implementing what you already know but, in the end, you can see how all your hard work pays off.",
-    topic:"JavaScript",
-    date:"7 july",
-    author:"erke"
-}];
-async function getElements (){
-    
-        const response = await fetch(' https://api.nytimes.com/svc/topstories/v2/home.json?api-key=gD9IU2QvCP1RIM33ArbttgsdVz8WnPkl');
-        const resJson = await response.json();
-        console.log(resJson.results);}
-     
-        const mainDiv = document.getElementById("Main");
+"Use sctric";
 
-model.map((item,) =>{
+async function getElements() {
+  const response = await fetch(
+    " https://api.nytimes.com/svc/topstories/v2/home.json?api-key=gD9IU2QvCP1RIM33ArbttgsdVz8WnPkl"
+  );
+  const resJson = await response.json();
+  console.log(resJson.results);
+  const res = resJson.results;
+  console.log(res);
+
+  const mainDiv = document.getElementById("Main");
+
+  res.map((item,_index) => {
     const html = `
     <div class="conteiner1">
     <div class="left">
         <div class="left01">    
             <img class="img2" src="./Img (2).png">
-        <div class="s1">${item.author}</div>    
+        <div class="s1">${item.byline}</div>    
         <div class="s2">in</div> 
         <div class="s3">Topics Name</div> 
         <div class="s4">.</div> 
-        <div class="s5">${item.date}</div> 
+        <div class="s5">${item.updated_date}</div> 
         </div>
         <div class="left02">${item.title}</div>
-        <div class="left03">${item.caption}</div>
+        <div class="left03">${item.selection}</div>
         <div class="left04">
             <div class="infoBottom">
-                <button class="but">${item.topic}</button>
+                <button class="but">${item.subsection}</button>
                 <div class="min">12 min read</div>
                 <div class="dot">·</div> 
                 <div class="select">Selected for you</div>
@@ -55,14 +41,14 @@ model.map((item,) =>{
 
         </div>
      </div>
-     <img class="img1" src="./Img.png">
+     <img class="img1" src="${item.multimedia[0].url}">
      </div>`;
-     
-mainDiv.innerHTML += html;
-});
+
+    mainDiv.innerHTML += html;
+  });
+}
 
 getElements();
-
 
 // const mainDiv = document.getElementById("Main");
 
@@ -70,13 +56,13 @@ getElements();
 //     const html = `
 //     <div class="conteiner1">
 //     <div class="left">
-//         <div class="left01">    
+//         <div class="left01">
 //             <img class="img2" src="./Img (2).png">
-//         <div class="s1">${item.author}</div>    
-//         <div class="s2">in</div> 
-//         <div class="s3">Topics Name</div> 
-//         <div class="s4">.</div> 
-//         <div class="s5">${item.date}</div> 
+//         <div class="s1">${item.author}</div>
+//         <div class="s2">in</div>
+//         <div class="s3">Topics Name</div>
+//         <div class="s4">.</div>
+//         <div class="s5">${item.date}</div>
 //         </div>
 //         <div class="left02">${item.title}</div>
 //         <div class="left03">${item.caption}</div>
@@ -84,7 +70,7 @@ getElements();
 //             <div class="infoBottom">
 //                 <button class="but">${item.topic}</button>
 //                 <div class="min">12 min read</div>
-//                 <div class="dot">·</div> 
+//                 <div class="dot">·</div>
 //                 <div class="select">Selected for you</div>
 //             </div>
 //             <div class="square">
@@ -92,7 +78,6 @@ getElements();
 //                 <img src="./Icon (1).png">
 //                 <img src="./Icon (1).png">
 //             </div>
-            
 
 //         </div>
 //      </div>
